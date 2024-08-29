36baeb8baa2bce81eaae18ef243ee11403f6b890
rashikolhapure <rashikolhapure@gmail.com>
2024-08-29 11:00:28 +0530

    Initial Commit

---
+++ expensetrackerapi/src/main/java/in/rashi/expensetrackerapi/controller/ExpenseController.java
@@ -0,0 +1,13 @@
+package in.rashi.expensetrackerapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpenseController {
+
+    @GetMapping("/expenses")
+    public String getAllExpenses() {
+        return "List of expenses";
+    }
+}
